4eae114245141abdfb4aef38b1f450d2bbc2a036
sk9288go <solkyu.park@junglim.com>
2023-01-31 23:51:40 +0900

    d
    d

--- pages/api/images.js
+++ pages/api/images.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { Configuration, OpenAIApi } = require("openai");
   const configuration = new Configuration({
-    apiKey: "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"
+    apiKey: "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"
     // req.query.t,
   });
   const openai = new OpenAIApi(configuration);
@@ -9,11 +9,7 @@ export default async function handler(req, res) {
     style: req.query.s,
     prompt: req.query.p + "," + req.query.s,
     n: parseInt(req.query.n),
-    size : req.query.si, 
-    // 이미지 사이즈 옵션 추가 216,512,620,max 지정 
-    // 프롬프트 = 주어, 목적어 위주의 
-    // 스타일 버튼, 예시가 될수있는 
-    // ? 버튼 hover시 설명 추가 
+    size : req.query.si,    
   });
   console.log(response.data.data);
 

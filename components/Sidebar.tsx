6fe62b0aedbb58e8d1c5bcdb1e7d105534bcc5bd
sk9288go <solkyu.park@junglim.com>
2023-02-23 15:31:11 +0900

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,33 +1,49 @@
 import { useState } from 'react';
 import styles from "./Sidebar.module.css";
+
+
 const Txt2Img = () => {
     const [prompt, setPrompt] = useState('');
-    const [width, setWidth] = useState('');
-    const [height, setHeight] = useState('');
-    const [numInferenceSteps, setNumInferenceSteps] = useState('800');
+    const [image_width, setimage_Width] = useState('512');
+    const [image_height, setHeight] = useState('512');
+    const [Steps, setSteps] = useState('50');
     const [image, setImage] = useState('');
+    const [seed, setseed] = useState('');
+
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
-        const response = await fetch('http://59.15.62.198:10002/txt2img', {
+
+        //const seed = useState(Math.random());
+        const randomSeed = Math.floor(Math.random()*100000).toString();
+        setseed(randomSeed);
+                
+        const response = await fetch('https://sad-pugs-help-34-87-136-216.loca.lt/text2img', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            prompt: encodeURIComponent(prompt),
-            height,
-            width,
-            num_inference_steps: numInferenceSteps
+            prompt: prompt, //encodeURIComponent(prompt),
+            image_height,
+            image_width,
+            num_inference_steps: Steps,
+            seed: randomSeed //encodeURIComponent(randomSeed)
         })
     });
-        const imageBlob = await response.blob();
-        const imageUrl = URL.createObjectURL(imageBlob);
-        setImage(imageUrl);
+    
+    
+    const imageData = await response.json();
+    const imageData1 = imageData.images[0];
+    const imageUrl = 'data:image/png;base64,' + imageData1;
+    setImage(imageUrl);
+
+
     } catch (error) {
-      console.error(error);
+    console.error(error);
     }
+
 };
 const handleDownload = () => {
     const link = document.createElement('a');
@@ -35,6 +51,7 @@ const handleDownload = () => {
     link.download = 'image.jpg';
     link.click();
 };
+
 
 return (    
     <div className={styles.Container1}>
@@ -55,30 +72,30 @@ return (
 
 
             <div className ={styles.inside_box}>           
-                <label htmlFor="width">Width:</label>
+                <label htmlFor="image_width">image_Width:</label>
                     <div className ={styles.help}>
                         <img src ="/Vector.png"/>            
                         <span className ={styles.helptext}>이미지의 가로 사이즈를 선택해주세요</span>
                     </div>
-                        <select className={styles.selectNom} id="width" value={width} onChange={(e) => setWidth(e.target.value)}>
+                        <select className={styles.selectNom} id="image_width" value={image_width} onChange={(e) => setimage_Width(e.target.value)}>
                         <option value="256">256</option>
                         <option value="512">512</option>
                         <option value="1024">1024</option>
                         </select>                    
                     </div>
             <div className ={styles.inside_box}>
-                <label htmlFor="height">Height:</label>
+                <label htmlFor="image_height">Height:</label>
                     <div className ={styles.help}>
                         <img src ="/Vector.png"/>            
                         <span className ={styles.helptext}>이미지의 세로 사이즈를 선택해주세요</span>
                     </div>
-                        <select className={styles.selectNom} id="height" value={height} onChange={(e) => setHeight(e.target.value)}>
+                        <select className={styles.selectNom} id="image_height" value={image_height} onChange={(e) => setHeight(e.target.value)}>
                         <option value="256">256</option>
                         <option value="512">512</option>
                         <option value="1024">1024</option>
                         </select>                    
             </div>
-
+            
 
             <div className ={styles.inside_box}>
             <label htmlFor="numInferenceSteps">Number of Inference Steps:</label>
@@ -86,17 +103,18 @@ return (
                     <img src ="/Vector.png"/>            
                     <span className ={styles.helptext}>이미지의 퀄리티에 영향을 미치는 inference step 입니다 <br/> 1부터 999까지 세팅가능하나 권장 범위는 200~600입니다.</span>
                 </div>
-                <input className ={styles.input} id="numInferenceSteps" value={numInferenceSteps} onChange={(e) => setNumInferenceSteps(e.target.value)} />
+                <input className ={styles.input} id="numInferenceSteps" value={Steps} onChange={(e) => setSteps(e.target.value)} />
             </div>        
     
         <button className={styles.button1}type="submit" >Generate</button>
         </form>
         </div>
 
-
+        
         
     <div className = {styles.node}>
-        {image ? (<div><img src={image} alt="Generated Image" onError={(e) => console.error(e)} />
+        {image ? (<div>
+            <img src={image} alt="Generated Image" onError={(e) => console.error(e)} />
             <br />
             <button onClick={handleDownload}>Download</button>
         </div>
